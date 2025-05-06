03f6adf2fd1e2f09c948d0f89ee8155ff004620f
miguelmunizzdev <miguelmunizzdev@gmail.com>
2025-05-05 22:30:14 -0300

    Update firebaseConfig.ts
    Limpeza Key

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,16 +1,1 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBm9nzBFXe5X6_MM7FmvLvUh5avvT6hE44",
-  authDomain: "desconet-8d482.firebaseapp.com",
-  projectId: "desconet-8d482",
-  storageBucket: "desconet-8d482.appspot.com",
-  messagingSenderId: "799067158158",
-  appId: "1:799067158158:android:bfd4345f8d57beb656bbdf"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
